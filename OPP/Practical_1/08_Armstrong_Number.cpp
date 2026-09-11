0469173c6288a8e7215543a58ff4c6d7208a23ee
ameyabankar <ameyabankar232009@gmail.com>
2026-09-11 12:55:13 +0530

    Add OPP Practical 1 - 08_Armstrong_Number.cpp

---
+++ OPP/Practical_1/08_Armstrong_Number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n, original, remainder, digits = 0, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    original = n;
+
+    while (n != 0) {
+        digits++;
+        n /= 10;
+    }
+
+    n = original;
+
+    while (n != 0) {
+        remainder = n % 10;
+        sum += pow(remainder, digits);
+        n /= 10;
+    }
+
+    if (sum == original)
+        cout << original << " is an Armstrong Number";
+    else
+        cout << original << " is not an Armstrong Number";
+
+    return 0;
+}
